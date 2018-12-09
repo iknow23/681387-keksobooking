88943076b3142f67decd10d7d3563db15a9e5dc7
Aynur <felix-xilef@mail.ru>
2018-12-09 09:57:37 +0300

    Сделал отступы внутри IIFE

--- js/map.js
+++ js/map.js
@@ -1,74 +1,73 @@
 (function () {
-'use strict';
+  'use strict';
 
-var ESC = 27;
-var map = document.querySelector('.map');
+  var ESC = 27;
+  var map = document.querySelector('.map');
 
-//  активация страницы, отключаю все элементы ввода формы
-var formElements = document.querySelectorAll('fieldset');
+  //  активация страницы, отключаю все элементы ввода формы
+  var formElements = document.querySelectorAll('fieldset');
 
-for (var j = 0; j < formElements.length; j++) {
-  formElements[j].disabled = true;
-}
-
-//  перевожу карту в активное состояния
-var mainPin = document.querySelector('.map__pin--main');
-var mainForm = document.querySelector('.ad-form');
-
-/**
- * скрытие карточки объявления
- */
-var deleteOpenedCard = function() {
-  var mapCard = document.querySelector('.map__card');
-  map.removeChild(mapCard);
-};
-
-/**
- * рендер карточек обяъвлений при нажатии на метки
- * @param  {number} pinId
- * @return {Object} newCard
- */
-var doCardJob = function(pinId) {
-  //  проверка наличия карточки
-  var elementAvailable = document.querySelector('.map__card');
-  if (elementAvailable) {
-    deleteOpenedCard();
+  for (var j = 0; j < formElements.length; j++) {
+    formElements[j].disabled = true;
   }
 
-  var newCard = window.card.renderCard(window.data.appartments[pinId]);
-  map.insertBefore(newCard, window.filter.filter);
-}
+  //  перевожу карту в активное состояния
+  var mainPin = document.querySelector('.map__pin--main');
+  var mainForm = document.querySelector('.ad-form');
 
-/**
- * рендер карточек объявлений при нажатии на метки
- */
-var addPinsClickHandler = function() {
-  var pinsList = window.pin.similarListElement.querySelectorAll('.map__pin:not(.map__pin--main)');
-  for (var i = 0; i < pinsList.length; i++) {
-    pinsList[i].addEventListener('click', function(evt) {
-      var button = evt.currentTarget;
-      var pinId = button.getAttribute('data-id');
-      doCardJob(pinId);
+  /**
+   * скрытие карточки объявления
+   */
+  var deleteOpenedCard = function() {
+    var mapCard = document.querySelector('.map__card');
+    map.removeChild(mapCard);
+  };
 
-      var popupCloseButton = document.querySelector('.popup__close');
-      popupCloseButton.addEventListener('click', function () {
-        deleteOpenedCard();
-      });
-      document.addEventListener('keydown', function (evt) {
-        if (evt.keyCode === ESC) {
+  /**
+   * рендер карточек обяъвлений при нажатии на метки
+   * @param  {number} pinId
+   * @return {Object} newCard
+   */
+  var doCardJob = function(pinId) {
+    //  проверка наличия карточки
+    var elementAvailable = document.querySelector('.map__card');
+    if (elementAvailable) {
+      deleteOpenedCard();
+    }
+
+    var newCard = window.card.renderCard(window.data.appartments[pinId]);
+    map.insertBefore(newCard, window.filter.filter);
+  }
+
+  /**
+   * рендер карточек объявлений при нажатии на метки
+   */
+  var addPinsClickHandler = function() {
+    var pinsList = window.pin.similarListElement.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (var i = 0; i < pinsList.length; i++) {
+      pinsList[i].addEventListener('click', function(evt) {
+        var button = evt.currentTarget;
+        var pinId = button.getAttribute('data-id');
+        doCardJob(pinId);
+
+        var popupCloseButton = document.querySelector('.popup__close');
+        popupCloseButton.addEventListener('click', function () {
           deleteOpenedCard();
-        }
+        });
+        document.addEventListener('keydown', function (evt) {
+          if (evt.keyCode === ESC) {
+            deleteOpenedCard();
+          }
+        });
       });
-    });
-  }
-};
+    }
+  };
 
-window.map = {
-  map: map,
-  formElements: formElements,
-  mainForm: mainForm,
-  addPinsClickHandler: addPinsClickHandler
-};
+  window.map = {
+    map: map,
+    formElements: formElements,
+    mainForm: mainForm,
+    addPinsClickHandler: addPinsClickHandler
+  };
 
 })();
-
